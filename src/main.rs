e7d39c8516b228722b2df4f96ced666838367ef1
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2026-03-30 02:01:26 +0100

    Add Proof of concept

--- src/main.rs
+++ src/main.rs
@@ -19,21 +19,38 @@ fn main() -> anyhow::Result<()> {
             let app = new_miasma_router(&CONFIG);
             eprintln!("{}\n", "Starting Miasma...".green());
 
-            tokio::spawn(check_for_new_version());
+            //tokio::spawn(check_for_new_version());
 
-            let addr = format!("{}:{}", CONFIG.host, CONFIG.port);
-            let listener = tokio::net::TcpListener::bind(&addr)
+            // This logic can probably be improved but it is a quick draft, and the compiler is
+            // happy with it, should add #[cfg(unix)] and add err handling
+            let is_unix_socket = CONFIG.host.starts_with("unix://");
+            if  is_unix_socket {
+                let addr = format!("{}", CONFIG.host.strip_prefix("unix://").unwrap());
+                let listener = tokio::net::UnixListener::bind(&addr)
+                .with_context(|| format!("Could not bind to {addr}").red())?;
+                eprintln!(
+                    "Listening on {} with {} max in-flight requests. Serving poisoned training data from {} with {} links per response...",
+                    addr.cyan(), CONFIG.max_in_flight.to_string().cyan(),
+                    CONFIG.poison_source.to_string().cyan(), CONFIG.link_count.to_string().cyan()
+                );
+                axum::serve(listener, app)
+                    .await
+                    .with_context(|| "server exited with an unexpected error".red())
+            } else {
+                let addr = format!("{}:{}", CONFIG.host, CONFIG.port);
+                let listener = tokio::net::TcpListener::bind(&addr)
                 .await
                 .with_context(|| format!("could not bind to {addr}").red())?;
+                eprintln!(
+                    "Listening on {} with {} max in-flight requests. Serving poisoned training data from {} with {} links per response...",
+                    addr.cyan(), CONFIG.max_in_flight.to_string().cyan(),
+                    CONFIG.poison_source.to_string().cyan(), CONFIG.link_count.to_string().cyan()
+                );
+                axum::serve(listener, app)
+                    .await
+                    .with_context(|| "server exited with an unexpected error".red())
+            }
 
-            eprintln!(
-                "Listening on {} with {} max in-flight requests. Serving poisoned training data from {} with {} links per response...",
-                addr.cyan(), CONFIG.max_in_flight.to_string().cyan(),
-                CONFIG.poison_source.to_string().cyan(), CONFIG.link_count.to_string().cyan()
-            );
 
-            axum::serve(listener, app)
-                .await
-                .with_context(|| "server exited with an unexpected error".red())
         })
 }
