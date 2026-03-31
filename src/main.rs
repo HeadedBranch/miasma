8c7abdfb168a5b5e8fe2a7883672e740b0736d60
Isaac Hesslegrave <isaac.hesslegrave@gmail.com>
2026-04-01 00:56:02 +0100

    Add Listener implementation to allow single declaration of listener

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,42 @@
 use anyhow::Context;
 use colored::Colorize;
 use std::sync::LazyLock;
+use tokio::net::{UnixListener, TcpListener, UnixStream, TcpStream};
+use tokio_util::either::Either;
 
 use miasma::{MiasmaConfig, check_for_new_version, new_miasma_router};
 
 static CONFIG: LazyLock<MiasmaConfig> = LazyLock::new(MiasmaConfig::new);
+
+// This is extremely ugly but it does work, should probably be moved somewhere
+// This allows both TcpListener and UnixListener to be used by axum::serve
+// It does this by implementing the trait it requires on a custom type
+// (needed because of orphan rules), I know it looks ugly I wrote it
+struct Listeners (Either<TcpListener, UnixListener>);
+
+impl axum::serve::Listener for Listeners {
+    type Io = Either<TcpStream, UnixStream>;
+    type Addr = Either<std::net::SocketAddr, tokio::net::unix::SocketAddr>;
+
+    async fn accept(&mut self) -> (Self::Io, Self::Addr) {
+        match &self.0 {
+            Either::Left(l) => {
+                let (stream, addr) = l.accept().await.unwrap();
+                (Either::Left(stream), Either::Left(addr))
+            }
+            Either::Right(r) => {
+                let (stream, addr) = r.accept().await.unwrap();
+                (Either::Right(stream), Either::Right(addr))
+            }
+        }
+    }
+    fn local_addr(&self) -> Result<Self::Addr, std::io::Error> {
+        match &self.0 {
+            Either::Left(l) => Ok(Either::Left(l.local_addr()?)),
+            Either::Right(r) => Ok(Either::Right(r.local_addr()?)),
+        }
+    }
+}
 
 fn main() -> anyhow::Result<()> {
     tokio::runtime::Builder::new_multi_thread()
@@ -18,28 +50,20 @@ fn main() -> anyhow::Result<()> {
 
             tokio::spawn(check_for_new_version());
 
-            // This logic can probably be improved but it is a quick draft, and the compiler is
-            // happy with it, should add #[cfg(unix)] and add err handling
             let addr = CONFIG.address();
-            if CONFIG.unix_socket {
-                let listener = tokio::net::UnixListener::bind(&addr)
-                    .with_context(|| format!("Could not bind to {addr}").red())?;
-
-                CONFIG.print_config_info();
-
-                axum::serve(listener, app)
-                    .await
-                    .with_context(|| "server exited with an unexpected error".red())
+            let listener: Listeners = if cfg!(unix) && CONFIG.unix_socket {
+                Listeners(Either::Right(UnixListener::bind(&addr)
+                    .with_context(|| format!("Could not bind to {addr}").red())?))
             } else {
-                let listener = tokio::net::TcpListener::bind(&addr)
+                Listeners(Either::Left(TcpListener::bind(&addr)
                     .await
-                    .with_context(|| format!("could not bind to {addr}").red())?;
+                    .with_context(|| format!("could not bind to {addr}").red())?))
+            };
 
-                CONFIG.print_config_info();
+            CONFIG.print_config_info();
 
-                axum::serve(listener, app)
-                    .await
-                    .with_context(|| "server exited with an unexpected error".red())
-            }
+            axum::serve(listener, app)
+                .await
+                .with_context(|| "server exited with an unexpected error".red())
         })
 }
